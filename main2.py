c92914542b48150c0b899afbb4b2567aa6d10699
Brittney-Williams <77993856+Brittney-Williams@users.noreply.github.com>
2021-02-01 21:54:02 -0500

    Complete Assignment

--- main2.py
+++ main2.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
-import sys
+import sys, random
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+
+
+
+number = random.randrange(1,11)
+count = 0
+guess = -1
+while guess != number:
+    count = count + 1
+    guess = input("Guess a number from 1 to 10 ")
+    try:
+     guess = int(guess)
+     if guess != number:
+        print("Not quite! Try and guess again")
+    except:
+        print("Try and answer a number buddy.")
+print("Great work! You recieve a gold star! You got the number of tries in " + str(count) + " tries. ")
